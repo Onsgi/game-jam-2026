4b79e4b10a1b3ae28dce2666b588b9ec3c1a1caf
Onsgi <90555592+Onsgi@users.noreply.github.com>
2026-02-01 02:22:30 +0200

    bard4

---
+++ scripts/bard4.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var label := $Label
+var full_text := "You can now double jump!"
+var speed := 0.04
+var index := 0
+
+func _ready():
+	label.text = ""
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		show_text()
+
+func show_text():
+	index = 0
+	label.text = ""
+	while index < full_text.length():
+		index += 1
+		label.text = full_text.substr(0, index)
+		await get_tree().create_timer(speed).timeout
